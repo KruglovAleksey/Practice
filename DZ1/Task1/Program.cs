f356bf8889aeef6116cbd4bf191416a07c231358
aleksey <razdolbam@yandex.ru>
2023-06-09 23:35:42 +0300

    Task 1,2,3,4

---
+++ DZ1/Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишите программу, которая на вход принимает два числа и выдает, какое число большее, а какое меньшее.
+
+Console.Write("Введите первое число: ");
+
+int num1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+
+int num2 = int.Parse(Console.ReadLine());
+
+if (num1 > num2)
+{
+    Console.WriteLine($"первое число больше второго {num1} > {num2}");
+}
+else
+{
+    Console.WriteLine($"второе число больше первого {num1} < {num2}");
+}
---
+++ DZ1/Task4/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.Write("Введите число n: ");
+
+double n = double.Parse(Console.ReadLine());
+
+if (n > 1)
+{   
+    for (int num = 2; num<n; num = num + 2)
+    {
+        Console.Write($"{num} ");
+    }
+}
+else
+{
+    Console.Write("В диапазоне от 1 до n нет четных чисел");
+}
