0c5d8342a1056e67e326c0078fc60cc97b203442
aleksey <razdolbam@yandex.ru>
2023-06-13 23:02:21 +0300

    Домашнее задание 2

---
+++ DZ2/Task2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.Write("Введите число ");
+int num = int.Parse(Console.ReadLine());
+
+int a = num;
+int index = 1;
+while (a / 10 > 0)
+{
+    a = a / 10;
+    index++;
+}
+if (index > 3)
+{
+    index = index - 3;
+    int b = num;
+    while (index > 0)
+    {
+        b = b / 10;
+        index--;
+    }
+Console.WriteLine(b % 10);
+}
+else
+{
+    Console.WriteLine("В числе меньше 3 чисел ");
+}
---
+++ DZ2/Task3/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+Console.Write("Введите число от 1 до 7: ");
+int num = int.Parse(Console.ReadLine());
+
+switch (num)
+{
+    case 1:
+    Console.WriteLine("Не выходной");
+    break;
+    case 2:
+    Console.WriteLine("Не выходной");
+    break;
+    case 3:
+    Console.WriteLine("Не выходной");
+    break;
+    case 4:
+    Console.WriteLine("Не выходной");
+    break;
+    case 5:
+    Console.WriteLine("Не выходной");
+    break;
+    case 6:
+    Console.WriteLine("Выходной");
+    break;
+    case 7:
+    Console.WriteLine("Выходной");
+    break;
+    default:
+    Console.WriteLine("Введенное число не входит в диапазон от 1 до 7");
+    break;
+}
+
