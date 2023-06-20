1186762e21379df7578f984af03255cd60ab7f57
aleksey <razdolbam@yandex.ru>
2023-06-20 22:53:26 +0300

    Домашняя работа 4

---
+++ DZ4/Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+int GetRes(int num, int st)
+{
+    int res = num;
+    for (int i = 0; i < st - 1; i++)
+    {
+        res = res * num;
+    }
+    return res;
+}
+
+Console.Write("Введите число А: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите число B: ");
+int b = int.Parse(Console.ReadLine());
+
+int pow = GetRes(a, b);
+Console.Write($"Число {a} в степени {b} = {pow}");
+
+
---
+++ DZ4/Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+int res = 0;
+for (int i = 0; number > 0; i++)
+{
+    res = res + number % 10;
+    number = number / 10;
+}
+
+Console.Write($"Сумма цифр числа равна {res}");
