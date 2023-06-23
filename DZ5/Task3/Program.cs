ee73a37ec806e464bb8e548788f2eff74d76e3c5
aleksey <razdolbam@yandex.ru>
2023-06-23 23:07:08 +0300

    Домашняя работа 5

---
+++ DZ5/Task3/Program.cs
@@ -0,0 +1,29 @@
+﻿/*Задайте массив вещественных чисел. 
+Найдите разницу между максимальным и минимальным элементов массива.*/
+
+double[] array = new double[5];
+Random rand = new Random();
+
+for(int i = 0; i < array.Length; i++)
+{
+    array[i] = Math.Round(rand.NextDouble()+rand.Next(0,10),2,MidpointRounding.ToZero);
+    Console.Write($"{array[i]} ");
+}
+double max = array[0];
+double min = array[0];
+for(int i = 0; i < array.Length; i++)
+{
+    if (max < array[i])
+    {
+        max = array[i];
+    }
+    if (min > array[i])
+    {
+        min = array[i];
+    }
+}
+
+Console.WriteLine();
+Console.WriteLine($"Максимальный элемент массива = {Math.Round(max,2,MidpointRounding.ToZero)}");
+Console.WriteLine($"Минимальный элемент массива = {Math.Round(min,2,MidpointRounding.ToZero)}");
+Console.WriteLine($"Разница между максимальным и минимальным элементов массива = {Math.Round(max - min,2,MidpointRounding.ToZero)}");
